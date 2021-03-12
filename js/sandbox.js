5bd70c434d6930044f8d94dcd1efb796cc727111
unknown <hassanchajai>
2021-03-12 11:16:27 +0100

    front end

--- js/sandbox.js
+++ js/sandbox.js
@@ -5,3 +5,30 @@ console.log("%cAlrights Reserved 2021", "color:orange");
 // and function for add 
 // and function for remove
 // and function for modifie
+
+//  books
+
+//  authour
+
+
+
+// login
+
+
+// test
+
+
+var x = "hello";
+fetch("php/test.php", {
+    method: 'get',
+
+
+
+}).then(function(response) {
+    console.log(response);
+    return response.json();
+}).then(res => {
+    console.log(res);
+}).catch(err => {
+    console.log(err);
+});
